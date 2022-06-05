8a28262ce53eea33d24d39b74da73ec8f41ffc70
ntabucejo <ntabucejo@gmail.com>
2022-06-05 16:20:51 +0800

    Setup nextauth, prisma and tailwindcss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
+  return (
+    <>
+      <SessionProvider session={session}>
+        <Head>
+          <title>Next NextAuth Prisma</title>
+          <meta
+            name="description"
+            content="A nextjs, nextauth, prisma sandbox"
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
+  )
 }
 
-export default MyApp
+export default App
---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,27 @@
+import NextAuth from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+import GithubProvider from 'next-auth/providers/github'
+import EmailProvider from 'next-auth/providers/email'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import prisma from '../../../prisma'
+
+export default NextAuth({
+  secret: process.env.SECRET,
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    // OAuth authentication providers
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+    }),
+    // GithubProvider({
+    //   clientId: process.env.GITHUB_ID,
+    //   clientSecret: process.env.GITHUB_SECRET,
+    // }),
+    // Sign in with passwordless email link
+    // EmailProvider({
+    //   server: process.env.MAIL_SERVER,
+    //   from: "<no-reply@example.com>",
+    // }),
+  ],
+})
